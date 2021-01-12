24cbb35d5475eb53da14ed198cea9e5c15cb481b
ali-hazime <a.hazime@outlook.com>
2021-01-12 17:38:39 -0500

    UI updates and more platforms
    Updated in game UI - moved score and made it larger and also moved jump charge bar.
    
    Added more platform prefabs.

--- Assets/Scripts/AttachToPlatform.cs
+++ Assets/Scripts/AttachToPlatform.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class AttachToPlatform : MonoBehaviour
 {
-
+    public PlayerController pc;
     public bool isEnabled = false;
+
+    private void Start()
+    {
+        if (pc == null)
+        {
+            pc = FindObjectOfType<PlayerController>();
+        }
+    }
     // Start is called before the first frame update
     void Update()
     {
         isEnabled = true;
     }
-
-
-
     
-        
     public void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player") && isEnabled)
+        if (other.gameObject.CompareTag("Player") && isEnabled && pc.isGrounded)
         {
 
             other.gameObject.transform.parent = transform;
